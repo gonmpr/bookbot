1e155ac65d9f6fba42a805ea1b8ee758a5cedff5
gonmpr <gmporrorosas@gmail.com>
2024-11-28 18:53:35 -0300

    just order the code in main.py

--- main.py
+++ main.py
@@ -1,13 +1,16 @@
 ### BookBot ###
 """
-Takes a a book as input, escifically the path to the book, and returns:
+Takes a book as an input, escifically the path to the book, and returns:
 
 - number of words in the book
--amountthe letter appears
+- amount of times the letters appears
 
 """
 
 book_path = "books/frankenstein.txt"
+
+
+
 
 def main():
 
@@ -28,6 +31,17 @@ def main():
         if word["letter"].isalpha():
             print(f"The {word["letter"]} character was found {word["times"]} times")
     print("\n--- End report ---")
+
+
+
+
+
+
+
+
+
+
+
 
 #get the text out of the book and make it usable by python, the parameter is the path of the book
 def get_book_text(book):
@@ -61,12 +75,15 @@ def count_letters(text):
     return letters
 
 
-def list_letters(dictionary):
-    list_letters = list()
-    for key,value in dictionary.items():
-          list_letters.append({"letter": key, "times": value})
+
+
+#transform the dictionary from {letter: times, letter: times, etc...} to ({letter: times}, {letter: times}, etc...)
+def list_letters(letters_dict_format):
+    letter_list_format = list()
+    for key,value in letters_dict_format.items():
+          letter_list_format.append({"letter": key, "times": value})
         
-    return list_letters
+    return letter_list_format
 
 
 
